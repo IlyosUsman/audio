3e3258e93cf6b7a83d7ba99a2d26c04378a06ffb
IlyosUsman <91330828+IlyosUsman@users.noreply.github.com>
2021-12-26 15:14:02 +0500

    frist commit

---
+++ js/voice.js
@@ -0,0 +1,48 @@
+const startButton = document.querySelector("#start")
+const recognition = new webkitSpeechRecognition();
+recognition.continous = false;
+recognition.lang = "Uz-uz";
+recognition.interimesult = false;
+recognition.maxAkternative = 1;
+const audioPlayer = document.getElementById('audio-player');
+
+const synth = window.speechSynthesis;
+
+startButton.addEventListener("click", () => {
+    recognition.start();
+});
+
+
+let utter = new SpeechSynthesisUtterance("Salom, ahvollar qaley?");
+utter.onend = () => {
+    recognition.start();
+};
+
+recognition.onresult = (arg) => {
+    const transcript = arg.results[arg.results.length - 1][0].transcript.trim();
+
+    if (transcript === "salom") {
+        recognition.stop();
+        utter.text = "Salom hayrli kun?";
+        synth.speak(utter);
+        console.log("Salom ahvollar qaley");
+    } else if (transcript === "") {
+        recognition.stop();
+        utter.text = "Hayr salomat boling";
+        synth.speak(utter);
+        console.log("Hayr salomat boling");
+    }
+
+}
+
+function fetcher(arg){
+    fetch(`https://internal.nutq.uz/api/v1/cabinet/synthesize/?t=${arg}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        return data
+    });
+}
+
+// console.log(fetcher('salom qalesiz'));
