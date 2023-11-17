c0797622ca619671ec9ff2595c7ea127173b9973
IlyosUsman <91330828+IlyosUsman@users.noreply.github.com>
2023-11-17 15:06:35 +0500

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -23,7 +23,7 @@ recognition.onresult = (arg) => {
 
     if (transcript === "salom") {
         recognition.stop();
-        utter.text = "Hello how are you?";
+        utter.text = "Salom ahvollar qalay?";
         synth.speak(utter);
         console.log("Salom ahvollar qaley");
     } 
